904317bd0ae19054f7bb33fe7c6297320c30dd4f
Gustavo Aguiar <gugahoa@gmail.com>
2018-06-27 04:08:21 -0300

    Starting Brave Clojure learning

---
+++ do-things/src/do_things/core.clj
@@ -0,0 +1,37 @@
+(ns do-things.core)
+
+(def asym-hobbit-body-parts [{:name "head" :size 3}
+                             {:name "left-eye" :size 1}
+                             {:name "left-ear" :size 1}
+                             {:name "mouth" :size 1}
+                             {:name "nose" :size 1}
+                             {:name "neck" :size 2}
+                             {:name "left-shoulder" :size 3}
+                             {:name "left-upper-arm" :size 3}
+                             {:name "chest" :size 10}
+                             {:name "back" :size 10}
+                             {:name "left-forearm" :size 3}
+                             {:name "abdomen" :size 6}
+                             {:name "left-kidney" :size 1}
+                             {:name "left-hand" :size 2}
+                             {:name "left-knee" :size 2}
+                             {:name "left-thigh" :size 4}
+                             {:name "left-lower-leg" :size 3}
+                             {:name "left-achilles" :size 1}
+                             {:name "left-foot" :size 2}])
+
+(defn matching-part
+  "Replace the beginning of :name part with right, if :name part starts with left"
+  [part]
+  {:name (clojure.string/replace (:name part) #"^left-" "right-"),
+   :size (:size part)})
+
+(defn symmetrize-body-parts
+  [asym-body-parts]
+  (set (into asym-body-parts
+         (map matching-part asym-body-parts))))
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
